78adbed31744ff457eadeaba56ea83d328c33fc7
Eggbertx <eggbertx@users.noreply.github.com>
2025-06-10 16:16:02 -0700

    Move action to first command line argument

--- cmd/territories-referee/main.go
+++ cmd/territories-referee/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"os"
+	"slices"
 
 	"github.com/Eggbertx/territories-game/pkg/actions"
 	"github.com/Eggbertx/territories-game/pkg/config"
@@ -13,7 +14,8 @@ import (
 )
 
 var (
-	logger zerolog.Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+	logger       zerolog.Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+	validActions                = []string{"join", "color", "raise", "move", "attack"}
 )
 
 func runningInTerminal() bool {
@@ -27,15 +29,35 @@ func runningInTerminal() bool {
 func main() {
 	var event actions.GameEvent
 	var jsonOutput bool
-	flag.BoolVar(&jsonOutput, "json", false, "output in JSON format, default is colorized text (for console)")
-	flag.StringVar(&event.Action, "action", "", "the action to be taken, must be join, move, or attack")
-	flag.StringVar(&event.Subject, "subject", "", "the subject of the action")
-	flag.StringVar(&event.Predicate, "predicate", "", "the target that the subject is going to be joining, moving to, attacking, etc")
-	flag.StringVar(&event.User, "user", "", "the user that is making the action")
-	flag.Parse()
 
-	err := config.InitLogger(jsonOutput)
+	err := config.InitLogger(false)
 	if err != nil {
+		logger.Fatal().Err(err).Caller().Send()
+	}
+	logger, err = config.GetLogger()
+	if err != nil {
+		logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+		logger.Fatal().Err(err).Caller().Send()
+	}
+
+	if len(os.Args) < 2 {
+		logger.Fatal().Msg("Usage: territories-referee join|color|raise|move|attack -user <user> -subject <subject> -predicate <predicate>")
+		os.Exit(1)
+	}
+	if !slices.Contains(validActions, os.Args[1]) {
+		logger.Fatal().Msgf("Invalid action '%s', valid actions are: %v", os.Args[1], validActions)
+		os.Exit(1)
+	}
+
+	event.Action = os.Args[1]
+	flagSet := flag.NewFlagSet("territories-referee", flag.ExitOnError)
+	flagSet.BoolVar(&jsonOutput, "json", false, "output in JSON format, default is colorized text (for console)")
+	flagSet.StringVar(&event.Subject, "subject", "", "the subject of the action")
+	flagSet.StringVar(&event.Predicate, "predicate", "", "the target that the subject is going to be joining, moving to, attacking, etc")
+	flagSet.StringVar(&event.User, "user", "", "the user that is making the action")
+	flagSet.Parse(os.Args[2:])
+
+	if err = config.InitLogger(jsonOutput); err != nil {
 		logger.Fatal().Err(err).Caller().Send()
 	}
 
@@ -46,7 +68,7 @@ func main() {
 	}
 
 	if event.Action == "" || event.User == "" {
-		logger.Fatal().Msg("action and user must be specified")
+		logger.Fatal().Msg("user must be specified")
 	}
 
 	db, err := db.GetDB()
